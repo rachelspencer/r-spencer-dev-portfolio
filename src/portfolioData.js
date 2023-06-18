8ed34055cb34243ddbae6abfb0be6321940c8bff
Rachel Spencer <rachelspencer688@gmail.com>
2023-06-18 14:17:53 -0400

    removing portfolioData from .gitignore

---
+++ src/portfolioData.js
@@ -0,0 +1,21 @@
+ const portfolioData = {
+   //for Header.js
+    name: "Rachel Spencer",
+    headerLogo: "https://s24193.pcdn.co/wp-content/uploads/2016/07/entity-women-that-did-ada-lovelace-1320x720.jpg",
+    //for Intro.js
+    introPara: "A Frontend focused Web Developer building responsive applications, with a passion for crafting intuitive user interfaces and delivering seamless user experiences.",
+    linkedin:"https://www.linkedin.com/in/rachel-spencer-87a539246/",
+    github: "",
+    twitter:"",
+    youtube:"",
+    //for AboutMe.js
+    aboutPara: "Here you will find more information about me and my current skills mostly in terms of programming and technology.",
+    paraOne: "My interest in Front End Development started in 2021. Free courses on Codecademy eventuated to my enrollment in Devmountains part time Web Development bootcamp (Aug 2021 - May 2023). It's fair to say I caught the bug, no pun intended.",
+    paraTwo: "I enjoy the process of designing and bringing wireframes to life using styling sheet languages like CSS and program languages like Javascript.",
+    paraThree: "Since my graduation I have continued to build on my skills by making applications in my spare time. I hope to gain the necessary skills that make a Junior Software engineer valuable in a team environment.",
+    //for Contact.js
+    contactPara: "Feel free to contact me by submitting the form below and I will get back to you as soon as possible.",
+ };
+
+
+ export default portfolioData;
