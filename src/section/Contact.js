8401692dbe104bcc4d2301a55e1ce209c90b85e1
Rachel Spencer <rachelspencer688@gmail.com>
2023-06-18 12:27:09 -0400

    emailjs added for contact form

--- src/section/Contact.js
+++ src/section/Contact.js
@@ -1,26 +1,25 @@
-import { useState } from 'react';
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
 import Panel from "../components/Panel";
 import Info from "../components/Info";
 import SectionTitles from "../components/SectionTitles";
 import Card from "../components/Card";
-import Button from '../components/Button';
-import portfolioData from '../portfolioData';
+import Button from "../components/Button";
+import portfolioData from "../portfolioData";
 
 function Contact(){
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [message, setMessage] = useState('')
+    const form = useRef();
 
-    const sendMessage = async () => {
-        // try {
-        //     await // set up with whatever db you are using (name, email, message)
-        // } catch (err){
-        //     console.log(err)
-        //     setName('')
-        //     setEmail('')
-        //     setMessage('')
-        // }
-    }
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_t55mz2c', 'template_37kdr46', form.current, 'gjqJBh7-YSrbGSMdb')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
 
     return (
         <Panel className='relative bg-gray-50 flex flex-col justify-center place-content-center dark:bg-black pb-8'>
@@ -29,22 +28,26 @@ function Contact(){
                 <SectionTitles>CONTACT</SectionTitles>
                     <Info className='flex justify-center text-center ml-10 mr-10 md:text-center md:text-xl md:ml-56 md:mr-56'>{portfolioData.contactPara}</Info> 
                     <Card className='bg-white rounded-lg md:translate-x-full dark:bg-slate-500 pl-16'>
-                        <h1 className='tracking-widest text-lg p-2 font-bold dark:text-indigo-500'>NAME</h1>
+                        <form ref={form} onSubmit={sendEmail}>
+                        <label className='tracking-widest text-lg p-2 font-bold dark:text-indigo-500'>NAME</label>
                         <input
-                        placeholder="Enter Your Name "
-                        onChange={(e)=>setName(e.target.value)}
+                        type="text"
+                        name="user_name"
+                        placeholder="Enter Your Name"
                         className='bg-slate-100 w-11/12 h-12 rounded-lg p-4 tracking-widest hover:ring hover:ring-amber-100 focus:outline-none focus:ring focus:ring-amber-300 dark:bg-black dark:focus:ring-indigo-500 dark:text-slate-400' />
-                        <h1 className='tracking-widest text-lg p-2 font-bold dark:text-indigo-500'>EMAIL</h1>
+                        <label className='tracking-widest text-lg p-2 font-bold dark:text-indigo-500'>EMAIL</label>
                         <input
-                        placeholder="Enter Your Email "
-                        onChange={(e)=>setEmail(e.target.value)}
+                        type="email"
+                        name="user_email"
+                        placeholder="Enter Your Email"
                         className='bg-slate-100 w-11/12 h-12 rounded-lg p-4 tracking-widest hover:ring hover:ring-amber-100 focus:outline-none focus:ring focus:ring-amber-300 dark:bg-black dark:focus:ring-indigo-500 dark:text-slate-400' />
-                        <h1 className='tracking-widest text-lg p-2 font-bold dark:text-indigo-500'>MESSAGE</h1>
+                        <label className='tracking-widest text-lg p-2 font-bold dark:text-indigo-500'>MESSAGE</label>
                         <textarea
-                        placeholder="Enter Your Message "
-                        onChange={(e)=>setMessage(e.target.value)}
+                        name="message"
+                        placeholder="Enter Your Message"
                         className='block p-2.5 bg-slate-100 w-11/12 h-56 rounded-lg p-4 tracking-widest hover:ring hover:ring-amber-100 focus:outline-none focus:ring focus:ring-amber-300 dark:bg-black dark:focus:ring-indigo-500 dark:text-slate-400' />
-                        <Button onClick={sendMessage}>SUBMIT</Button>
+                        <Button type="submit">SUBMIT</Button>
+                        </form>
                     </Card>
             </section>
             
