4a030862f2e6fcd0e6a121dc97f7329f14a0158b
Rachel Spencer <rachelspencer688@gmail.com>
2023-08-15 21:49:40 -0400

    add B Doggett image and testimonial

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { HiChevronDoubleLeft, HiChevronDoubleRight } from "react-icons/hi";
 import polly89 from '../images/polly89.png'
+import BenDoggett from '../images/BenDoggett.jpeg'
 
 function ImageSlider(){
     const [currentTestimonialIndx, setCurrentTestimonialIndx] = useState(0);
@@ -12,9 +13,9 @@ function ImageSlider(){
             quote:"I had the pleasure of collaborating with Rachel on a project. We studied together at DevMountain, and her passion and commitment to the program were evident from day one. She caught on to new technologies quickly and was always incredibly insightful in class. That, along with her attention to detail, and artistic designs, pushed me to reach out and collaborate with her on a project. And I wasn't disappointed! She's extremely motivated and always stays on task. We completed our project in just a few weeks, and I hope to collaborate with her on many more projects.",
         },
         {
-            image:'https://static.independent.co.uk/s3fs-public/thumbnails/image/2017/09/27/08/jennifer-lawrence.jpg',
-            name:'Jennifer Lawrence',
-            quote:`Let the games begin! Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
+            image: BenDoggett,
+            name:'Ben Doggett',
+            quote: "I had the incredible privilege of mentoring Rachel as a developer during her time at Devmountain. I have consistently found myself amazed by her unwavering dedication, infectious enthusiasm, and remarkable growth in the field. Throughout our time together, Rachel has demonstrated exceptional proficiency in a diverse range of programming languages, libraries, and tools, notably including JavaScript and React.js. Her comprehensive understanding of both front-end and back-end technologies is truly commendable, and her insatiable thirst for knowledge never ceases to impress me.",
         },
     ]
 
