06d76a2229c13b4d321f1432b07513eaeca6d7ad
Rachel Spencer <rachelspencer688@gmail.com>
2023-08-22 13:42:35 -0400

    add Garrett testimonial

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import { HiChevronDoubleLeft, HiChevronDoubleRight } from "react-icons/hi";
-import polly89 from '../images/polly89.png'
-import BenDoggett from '../images/BenDoggett.jpeg'
+import polly89 from '../images/polly89.png';
+import BenDoggett from '../images/BenDoggett.jpeg';
+import Garrett from '../images/Garrett.png';
 
 function ImageSlider(){
     const [currentTestimonialIndx, setCurrentTestimonialIndx] = useState(0);
@@ -14,9 +15,14 @@ function ImageSlider(){
         },
         {
             image: BenDoggett,
-            name:'Ben Doggett',
+            name:"Ben Doggett",
             quote: "I had the incredible privilege of mentoring Rachel as a developer during her time at Devmountain. I have consistently found myself amazed by her unwavering dedication, infectious enthusiasm, and remarkable growth in the field. Throughout our time together, Rachel has demonstrated exceptional proficiency in a diverse range of programming languages, libraries, and tools, notably including JavaScript and React.js. Her comprehensive understanding of both front-end and back-end technologies is truly commendable, and her insatiable thirst for knowledge never ceases to impress me.",
         },
+        {
+            image: Garrett,
+            name: "Garrett Y",
+            quote:"I am thrilled to endorse Rachel, a standout student at Devmountain. Her unwavering commitment and exceptional work ethic set her apart. Rachel's enthusiasm for learning React, Node, Express, and SQL was evident as she consistently went the extra mile to grasp each concept fully. She's not only a diligent problem solver but also an innovative thinker, infusing creativity into her projects. Rachel's collaborative spirit, attention to detail, and proficiency within the web development stack make her an asset to any team. Without hesitation, I recommend Rachel for any web developer role—her remarkable dedication and skill set ensures her success in the field."
+        }
     ]
 
     const prev = () => {
